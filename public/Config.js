86756c147bbc6438a275dd035e23ceb9498ff6b4
Remigiusz48749 <dsw48749@student.dsw.deu.pl>
2025-04-17 18:09:42 +0200

    Changed location of firebase connfig and added signin Funcion(not fully implemented)

--- public/Config.js
+++ public/Config.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//onst analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 export const googleAuthProvider = new GoogleAuthProvider(); 
 
